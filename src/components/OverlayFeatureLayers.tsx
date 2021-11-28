33f5ec0b735787b4061b263e143d885b9e5e560c
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-11-28 19:23:03 +0900

    Use leaflet-color-markers

--- src/components/OverlayFeatureLayers.tsx
+++ src/components/OverlayFeatureLayers.tsx
@@ -2,6 +2,7 @@ import ReactDOMServer from "react-dom/server";
 import { Feature, FeatureCollection } from "geojson";
 import { GeoJSON, LayersControl } from "react-leaflet";
 import styles from "./OverlayFeatureLayers.module.css";
+import L from "leaflet";
 
 type Props = {
   featureCollections: {
@@ -10,6 +11,18 @@ type Props = {
 };
 
 export function OverlayFeatureLayers({ featureCollections }: Props) {
+  const iconColors = [
+    "blue",
+    "gold",
+    "red",
+    "green",
+    "orange",
+    "yellow",
+    "violet",
+    "gray",
+    "black",
+  ];
+
   const FeaturePopup = ({ feature }: { feature: Feature }) => {
     return (
       <>
@@ -30,23 +43,31 @@ export function OverlayFeatureLayers({ featureCollections }: Props) {
 
   return (
     <>
-      {Object.entries(featureCollections).map(([name, featureCollection]) => {
-        return (
-          <LayersControl.Overlay key={name} name={name}>
-            <GeoJSON
-              data={featureCollection}
-              onEachFeature={(feature, layer) => {
-                // https://stackoverflow.com/a/60686195
-                layer.bindPopup(
-                  ReactDOMServer.renderToString(
-                    <FeaturePopup feature={feature} />
-                  )
-                );
-              }}
-            />
-          </LayersControl.Overlay>
-        );
-      })}
+      {Object.entries(featureCollections).map(
+        ([name, featureCollection], i) => {
+          const iconColor = iconColors[i % iconColors.length];
+          const icon = new L.Icon.Default({
+            iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${iconColor}.png`,
+            iconRetinaUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${iconColor}.png`,
+          });
+
+          return (
+            <LayersControl.Overlay key={name} name={name}>
+              <GeoJSON
+                data={featureCollection}
+                pointToLayer={(pointFeature, latlng) => {
+                  return L.marker(latlng, { icon: icon }).bindPopup(
+                    // https://stackoverflow.com/a/60686195
+                    ReactDOMServer.renderToString(
+                      <FeaturePopup feature={pointFeature} />
+                    )
+                  );
+                }}
+              />
+            </LayersControl.Overlay>
+          );
+        }
+      )}
     </>
   );
 }
