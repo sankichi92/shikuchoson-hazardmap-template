c8b4aacae5ef4670c24828a266562e17d9efa6d4
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-11-29 23:34:16 +0900

    Use react-helmet
    close #13

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { LatLngBounds } from "leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet/dist/leaflet.css";
+import { Helmet } from "react-helmet";
 import {
   LayersControl,
   MapContainer,
@@ -11,9 +12,9 @@ import {
 import { CityBoundary } from "./components/CityBoundary";
 import { OverlayFeatureLayers } from "./components/OverlayFeatureLayers";
 import { OverlayTileLayers } from "./components/OverlayTileLayers";
-import config from "./generated/hazardmap-config.json";
 import cityOsm from "./generated/city-osm.json";
 import featureCollections from "./generated/feature-collections.json";
+import config from "./generated/hazardmap-config.json";
 
 function App() {
   const bounds = cityOsm.elements[0].bounds;
@@ -23,27 +24,33 @@ function App() {
   );
 
   return (
-    <MapContainer
-      bounds={latLngBounds}
-      maxBounds={latLngBounds}
-      minZoom={5}
-      maxZoom={17}
-      style={{ height: "100vh" }}
-    >
-      <TileLayer
-        url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
-        attribution='<a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-      />
-      <ScaleControl position="bottomright" />
-      <CityBoundary cityOsm={cityOsm} />
-      <LayersControl position="topright" collapsed={window.innerWidth <= 768}>
-        <OverlayTileLayers tiles={config.tiles} />
-        <OverlayFeatureLayers
-          // @ts-ignore
-          featureCollections={featureCollections}
+    <>
+      <Helmet>
+        <title>{config.city}ハザードマップ</title>
+      </Helmet>
+
+      <MapContainer
+        bounds={latLngBounds}
+        maxBounds={latLngBounds}
+        minZoom={5}
+        maxZoom={17}
+        style={{ height: "100vh" }}
+      >
+        <TileLayer
+          url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
+          attribution='<a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
         />
-      </LayersControl>
-    </MapContainer>
+        <ScaleControl position="bottomright" />
+        <CityBoundary cityOsm={cityOsm} />
+        <LayersControl position="topright" collapsed={window.innerWidth <= 768}>
+          <OverlayTileLayers tiles={config.tiles} />
+          <OverlayFeatureLayers
+            // @ts-ignore
+            featureCollections={featureCollections}
+          />
+        </LayersControl>
+      </MapContainer>
+    </>
   );
 }
 
