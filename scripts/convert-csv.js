238eb7c37e7027195b886d71a26281db4dd1c9c0
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-11-30 23:04:13 +0900

    Fix convert-csv.js

--- scripts/convert-csv.js
+++ scripts/convert-csv.js
@@ -8,7 +8,7 @@ const res = {};
 
 const files = fs.readdirSync(path.join(__dirname, "../csv"));
 files.sort().forEach((file) => {
-  const name = file.match(fileNameRegexp).groups.name
+  const name = path.parse(file).name.match(fileNameRegexp).groups.name
   const csv = fs.readFileSync(path.join(__dirname, `../csv/${file}`));
   csv2geojson.csv2geojson(csv.toString(), (err, data) => {
     if (err) {
